17f694c1a84f17d3dba7a1f713359e2fa6c07f48
Vitaly <w.182@mail.ru>
2022-01-31 00:33:58 +0300

    Props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,12 +11,8 @@ const MyPosts = () => {
                 <button>Remove post</button>
             </div>
             <div className={cl.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message="Hi, what's up?" likes='15' />
+                <Post message='The best or nothing' likes='25' />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,17 @@
 import cl from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={cl.item}>
             <img src='https://avatars.mds.yandex.net/i?id=06ad5aa77b521c6323be2ad602e04fb0-4965727-images-thumbs&n=13&exp=1' />
-            post 1
+            {props.message}
             <div>
                 <span>like</span>
+                <div>
+                    Amounts of likes
+                    {props.likes}
+                </div>
             </div>
-
         </div>
     )
 }
