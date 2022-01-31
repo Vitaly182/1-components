9b507145ca6171dee08d9cf7aa9eeabf0c316bef
Vitaly <w.182@mail.ru>
2022-02-01 00:40:53 +0300

    routers

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,19 @@ const Navbar = () => {
     return (
         <nav className={cl.nav}>
             <div className={cl.item}>
-                <a>Profile</a>
+                <a href='/profile'>Profile</a>
             </div>
             <div className={cl.item}>
-                <a>Messages</a>
+                <a href='/dialogs'>Messages</a>
             </div>
             <div className={cl.item}>
-                <a>News</a>
+                <a href='/news'>News</a>
             </div>
             <div className={cl.item}>
-                <a>Music</a>
+                <a href='/music'>Music</a>
             </div>
             <div className={cl.item}>
-                <a>Settings</a>
+                <a href='/settings'>Settings</a>
             </div>
         </nav>
     )
