d7622d4ae44a77f4929816786cfda09ce9051e4f
Vitaly <w.182@mail.ru>
2022-02-01 03:31:22 +0300

    page Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,23 @@ import cl  from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={cl.dialogs}>
+            <div className={cl.dialogsItems}>
+                <div className={cl.dialog + ' ' + cl.active}>
+                    Vitaly
+                </div>
+                <div className={cl.dialog}>
+                    Max
+                </div>
+                <div className={cl.dialog}>
+                    Bork
+                </div>
+            </div>
+            <div className={cl.messages}>
+                <div className={cl.message}>Hi</div>
+                <div className={cl.message}>Who's that ?</div>
+                <div className={cl.message}>Nothing</div>
+            </div>
         </div>
     );
 };
