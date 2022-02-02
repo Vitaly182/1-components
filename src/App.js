b5bd738b437848dd69605f94d0094e3014621334
Vitaly <w.182@mail.ru>
2022-02-03 00:54:14 +0300

    change place BrowserRouter

--- src/App.js
+++ src/App.js
@@ -11,22 +11,19 @@ import Settings from './components/Settings/Settings';
 
 function App(props) {
   return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-        <Header/>
-        <Navbar state={props.state.sitebar}/>
-        <div className='app-wrapper-content'>
-          <Routes>
-            <Route path="/profile/*" element={<Profile state={props.state.profilePage}/>} />
-            <Route path="/dialogs/*" element={<Dialogs state={props.state.dialogsPage}/>} />
-            <Route path="/news/*" element={<News/>} />
-            <Route path="/music/*" element={<Music/>} />
-            <Route path="/settings/*" element={<Settings/>} />
-          </Routes>
-        </div>
-      </div>    
-    </BrowserRouter>
-
+    <div className='app-wrapper'>
+      <Header/>
+      <Navbar state={props.state.sitebar}/>
+      <div className='app-wrapper-content'>
+        <Routes>
+          <Route path="/profile/*" element={<Profile state={props.state.profilePage}/>} />
+          <Route path="/dialogs/*" element={<Dialogs state={props.state.dialogsPage}/>} />
+          <Route path="/news/*" element={<News/>} />
+          <Route path="/music/*" element={<Music/>} />
+          <Route path="/settings/*" element={<Settings/>} />
+        </Routes>
+      </div>
+    </div>    
   );
 }
 
