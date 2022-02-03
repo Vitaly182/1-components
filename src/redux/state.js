308f528e32a54b3260b791b25b68efe2a76fbe43
Vitaly <w.182@mail.ru>
2022-02-03 03:09:26 +0300

    throw callback through props

--- src/redux/state.js
+++ src/redux/state.js
@@ -38,8 +38,16 @@ let state = {
             { id:3, name: 'Lind'}
         ]
     }
-    
+}
 
+export let addPost = (postMessage) => {
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likesCount: 0
+    };
+
+    state.profilePage.posts.push(newPost);
 }
 
 export default state;
