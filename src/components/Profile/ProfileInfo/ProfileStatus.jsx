9526957528950d6453618607f57b6b9895c8093f
Vitaly <w.182@mail.ru>
2022-02-15 16:57:49 +0300

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -28,6 +28,12 @@ class ProfileStatus extends React.Component {
         })
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({ status: this.props.status })
+        }
+    }
+
     render() {
         return (
             <div>
