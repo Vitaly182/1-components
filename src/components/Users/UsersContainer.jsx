79263b39f64c02ce92d69041c83c1700bbe1728d
Vitaly <w.182@mail.ru>
2022-02-18 04:43:07 +0300

    reselect part 2

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -5,7 +5,7 @@ import Users from './Users';
 import Preloader from '../Common/Preloader/Preloader';
 import {withAuthRedirect} from './../../hoc/withAuthRedirect'
 import { compose } from 'redux';
-import { getUsers, getCurrentPage, getFollowingInProgress, getIsFetching, getPageSize, getTotalUsersCount } from '../../redux/users-selectors';
+import { getUsers, getUsersSuperSelector, getCurrentPage, getFollowingInProgress, getIsFetching, getPageSize, getTotalUsersCount } from '../../redux/users-selectors';
 
 
 class UsersContainer extends React.Component {
@@ -42,6 +42,7 @@ class UsersContainer extends React.Component {
 let mapStateToProps = (state) => {
     return {
         users: getUsers(state),
+        // users: getUsersSuperSelector(state),
         pageSize: getPageSize(state),
         totalUsersCount: getTotalUsersCount(state),
         currentPage: getCurrentPage(state),
